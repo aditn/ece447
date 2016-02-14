b0fe02c7528bd97fe7677cc1a36523d579076ef9
Tony Jin <tdjin@ece001.campus.ece.cmu.local>
2016-02-14 16:51:12 -0500

    added mux to stall pc

--- lab2/inputs/1test.s
+++ lab2/inputs/1test.s
@@ -1,5 +1,6 @@
 		.text
 
 __start:        addiu $t0, $zero, 10
-                addiu $t1, $zero, 0x1
+                addiu $t1, $t0, 0x1
+                addiu $t2, $t1, 0x2
                 syscall
