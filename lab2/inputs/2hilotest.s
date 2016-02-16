dbb5982dcf021ee543795d0cf8958bab3373289a
Adit Namdev <anamdev@ece001.campus.ece.cmu.local>
2016-02-15 20:51:34 -0500

    works???

--- lab2/inputs/2hilotest.s
+++ lab2/inputs/2hilotest.s
@@ -2,8 +2,11 @@
 
 __start:    addi $3, $zero, 1
 			mthi $3
-			nop
 			mfhi $4
-			addiu $t0, $zero, 10
+			addiu $4, $4, 10
+			mtlo $3
+			mflo $5
+			addiu $4, $4, 10
+			addi $6, $5 -2
             syscall
 
