11b28c8d089eef706a88cc667e3edcf5231255e2
Tony Jin <tdjin@ece001.campus.ece.cmu.local>
2016-04-05 21:20:28 -0400

    forwarding module

--- lab4/src/mips_core.sv
+++ lab4/src/mips_core.sv
@@ -989,39 +989,80 @@ endmodule
 ////
 //// forwardData: module for forwarding data to prevent RAW hazards
 ////
-//// wr_reg_EX, wr_reg_MEM, wr_reg_WB (inputs) - are reg numbers at different stages
+//// wr_reg_EX, wr_reg_MEM (inputs) - are reg numbers at different stages
 //// dcd_rs, dcd_rt (inputs) - the registers the next instruction is reading
-//// mem_write_en_EX, mem_write_en_MEM, mem_write_en (inputs) - the memory write enable bits at different stages
-//// ctrl_we_EX, ctrl_we_MEM, ctrl_we_WB (inputs) - register write enable bits at different stages
+//// ctrl_we_EX, ctrl_we_MEM (inputs) - register write enable bits at different stages
 //// rsfwd, rtfwd (output) - selects what data to forward to rs and rt data outputs
 ////
 
 module forwardData(
-  input logic [4:0] wr_reg_EX, wr_reg_MEM, wr_reg_WB, dcd_rt, dcd_rs,
-  input logic [3:0] mem_write_en_EX, mem_write_en_MEM, mem_write_en,
-  input logic ctrl_we_EX, ctrl_we_MEM, ctrl_we_WB,
-  output logic [1:0] rsfwd, rtfwd);
+  //input logic [31:0] pc_ID_1, pc_EX_1, pc_MEM_1, pc_ID_2, pc_EX_2, pc_MEM_2,
+  input logic [4:0] wr_reg_EX_1, wr_reg_MEM_1, dcd_rt_1, dcd_rs_1,
+  input logic [4:0] wr_reg_EX_2, wr_reg_MEM_2, dcd_rt_2, dcd_rs_2,
+  input logic ctrl_we_EX_1, ctrl_we_MEM_1, ctrl_we_EX_2, ctrl_we_MEM_2,
+  output logic [1:0] rsfwd_1, rtfwd_1, rsfwd_2, rtfwd_2);
 
   always_comb begin
-    rsfwd = 2'b0;
-    rtfwd = 2'b0;
-    if (ctrl_we_MEM != 0) begin
-      if ((dcd_rs != 0) && (dcd_rs == wr_reg_MEM)) begin
-        rsfwd = 2'b10;
+    rsfwd_1 = 2'b0;
+    rtfwd_1 = 2'b0;
+    rsfwd_2 = 2'b0;
+    rtfwd_2 = 2'b0;
+    if (ctrl_we_MEM_1 != 0) begin
+      if ((dcd_rs_1 != 0) && (dcd_rs_1 == wr_reg_MEM_1)) begin
+        rsfwd_1 = 2'b11;
       end
-      if ((dcd_rt != 0) && (dcd_rt == wr_reg_MEM)) begin
-        rtfwd = 2'b10;
+      if ((dcd_rt_1 != 0) && (dcd_rt_1 == wr_reg_MEM_1)) begin
+        rtfwd_1 = 2'b11;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rs_2 == wr_reg_MEM_1)) begin
+        rsfwd_2 = 2'b11;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rt_2 == wr_reg_MEM_1)) begin
+        rtfwd_2 = 2'b11;
       end
     end
-    if (ctrl_we_EX != 0) begin
-      if ((dcd_rs != 0) && (dcd_rs == wr_reg_EX)) begin
-        rsfwd = 2'b01;
+    if (ctrl_we_MEM_2 != 0) begin
+      if ((dcd_rs_1 != 0) && (dcd_rs_1 == wr_reg_MEM_2)) begin
+        rsfwd_1 = 2'b10;
       end
-      if ((dcd_rt != 0) && (dcd_rt == wr_reg_EX)) begin
-        rtfwd = 2'b01;
+      if ((dcd_rt_1 != 0) && (dcd_rt_1 == wr_reg_MEM_2)) begin
+        rtfwd_1 = 2'b10;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rs_2 == wr_reg_MEM_2)) begin
+        rsfwd_2 = 2'b10;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rt_2 == wr_reg_MEM_2)) begin
+        rtfwd_2 = 2'b10;
       end
     end
-
+    if (ctrl_we_EX_1 != 0) begin
+      if ((dcd_rs_1 != 0) && (dcd_rs_1 == wr_reg_EX_1)) begin
+        rsfwd_1 = 2'b01;
+      end
+      if ((dcd_rt_1 != 0) && (dcd_rt_1 == wr_reg_EX_1)) begin
+        rtfwd_1 = 2'b01;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rs_2 == wr_reg_EX_1)) begin
+        rsfwd_2 = 2'b01;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rt_2 == wr_reg_EX_1)) begin
+        rtfwd_2 = 2'b01;
+      end
+    end
+    if (ctrl_we_EX_2 != 0) begin
+      if ((dcd_rs_1 != 0) && (dcd_rs_1 == wr_reg_EX_2)) begin
+        rsfwd_1 = 2'b01;
+      end
+      if ((dcd_rt_1 != 0) && (dcd_rt_1 == wr_reg_EX_2)) begin
+        rtfwd_1 = 2'b01;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rs_2 == wr_reg_EX_2)) begin
+        rsfwd_2 = 2'b01;
+      end
+      if ((dcd_rs_2 != 0) && (dcd_rt_2 == wr_reg_EX_2)) begin
+        rtfwd_2 = 2'b01;
+      end
+    end
   end
 
 endmodule
@@ -1029,12 +1070,15 @@ endmodule
 ////
 //// stallDetector: module for enabling stalls if RAW, WAR, WAW hazard
 ////
+//// pc_ID, pc_EX (inputs) - pc values at different stages
 //// wr_reg_EX, wr_reg_MEM (inputs) - are reg numbers at different stages
 //// dcd_rs, dcd_rt (inputs) - the registers the next instruction is reading
-//// mem_write_en_EX, mem_write_en_MEM (inputs) - the memory write enable bits at different stages
-//// ctrl_we_EX, ctrl_we_MEM (inputs) - register write enable bits at different stages
+//// regdst (input) - destination register
+//// mem_en (input) - the memory write enable bits at ID stage
+//// ctrl_we, ctrl_we_EX (inputs) - register write enable bits at different stages
 //// stall (input) - signal that indicates an instruction is currently being stalled
-//// IFen, IDen, EXen, (output) - register enable bits at the IF, ID, and EX stages
+//// IFen, IDen, EXen (outputs) - register enable bits at the IF, ID, and EX stages
+//// CDen (output) - enable bit for countdown register
 //// CDAmt (output) - number of clock cycles to stall
 ////
 module stallDetector(
@@ -1042,7 +1086,7 @@ module stallDetector(
   input logic [4:0] wr_reg_1, wr_reg_EX_1, wr_reg_MEM_1, dcd_rt_1, dcd_rs_1,
   input logic [3:0] mem_en_1,
   input logic ctrl_we_1, ctrl_we_EX_1, regdst_1,
-  input logic [31:0] pc_ID_2, pc_EX_2, pc_MEM_2,
+  input logic [31:0] pc_ID_2, pc_EX_2,
   input logic [4:0] wr_reg_2, wr_reg_EX_2, wr_reg_MEM_2, dcd_rt_2, dcd_rs_2,
   input logic [3:0] mem_en_2,
   input logic ctrl_we_2, ctrl_we_EX_2, regdst_2, 
